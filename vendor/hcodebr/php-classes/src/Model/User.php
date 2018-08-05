3a4635d5c84bdc84647479519042376fed75414f
Sílvio Dias <esisildi@gmail.com>
2018-08-05 23:49:38 +0100

    Editar - Delete

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -77,21 +77,7 @@ use \Hcode\Model;
 		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
 	}
 
-	public function get($iduser)
-{
- 
- $sql = new Sql();
- 
- $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
- ":iduser"=>$iduser
- ));
- 
- $data = $results[0];
- 
- $this->setData($data);
- 
- }
-
+	
 
  public function save(){
 
@@ -118,6 +104,54 @@ pinadmin TINYINT*/
  	$this->setData($results[0]);
 
  }
+
+public function get($iduser)
+{
+ 
+ $sql = new Sql();
+ 
+ $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+ ":iduser"=>$iduser
+ ));
+ 
+ $data = $results[0];
+ 
+ $this->setData($data);
+ 
+ }
+
+public function update(){
+
+
+$sql = new Sql();
+
+
+ 	$results = $sql->select("Call sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",array(
+ 		":iduser"=>$this->getiduser(),
+ 		":desperson"=>$this->getdesperson(),
+ 		":deslogin"=>$this->getdeslogin(),
+ 		":despassword"=>$this->getdespassword(),
+ 		"desemail"=>$this->getdesemail(),
+ 		":nrphone"=>$this->getnrphone(),
+ 		":inadmin"=>$this->getinadmin()
+
+ 	));
+
+ 	$this->setData($results[0]);
+
+}
+
+public function delete(){
+
+	$sql = new Sql();
+
+	$sql->query("CALL sp_users_delete(:iduser)",array(
+		":iduser"=>$this->getiduser()
+
+	));
+
+}
+
 
 	}
 ?>
